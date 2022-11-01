9a8b5ebf11dd973e8b44d9850e38742815511a20
KOSA <KOSA@DESKTOP-G1POK2Q>
2022-11-01 14:54:23 +0900

    ;.

--- src/main/java/com/kosa/instagram/HomeController.java
+++ src/main/java/com/kosa/instagram/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Handles requests for the application home page.,amsnx,asnx
+ * Handles requests for the application home page.,amsnx,asnxg
  */
 @Controller
 public class HomeController {
