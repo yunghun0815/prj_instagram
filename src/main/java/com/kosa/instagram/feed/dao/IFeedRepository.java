aee98062963cb63a079db8b1687dd48c1008cc4a
KOSA <KOSA@DESKTOP-G1POK2Q>
2022-11-02 15:20:50 +0900

    0

--- src/main/java/com/kosa/instagram/feed/dao/IFeedRepository.java
+++ src/main/java/com/kosa/instagram/feed/dao/IFeedRepository.java
@@ -1,5 +1,18 @@
 package com.kosa.instagram.feed.dao;
 
-public class IFeedRepository {
+import java.util.List;
 
+import com.kosa.instagram.feed.model.FeedVo;
+import com.kosa.instagram.member.model.MemberVo;
+
+public interface IFeedRepository {
+
+	void countContent(String memberId);
+	void countFollower(String memberId);
+	void countFollow(String memberId);
+	List<FeedVo> selectContentListByUser(String memberId);
+	List<MemberVo> selectFollowerByUser(String memberId);
+	List<MemberVo> selectFollowByUser(String memberId);
+	
+	
 }
