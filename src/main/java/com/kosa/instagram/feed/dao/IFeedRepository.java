1041164688a5432afbe9ac889b5627ad7831f794
ujin <ujin@DESKTOP-NSV6N9P>
2022-11-02 15:17:43 +0900

    ;)

--- src/main/java/com/kosa/instagram/feed/dao/IFeedRepository.java
+++ src/main/java/com/kosa/instagram/feed/dao/IFeedRepository.java
@@ -1,5 +1,8 @@
 package com.kosa.instagram.feed.dao;
 
-public class IFeedRepository {
-
+public interface IFeedRepository {
+	void writeReply(int feedNo, String replyContent);
+	void deleteReply(int replyNo);
+	void likeFeed(int feedNo);
+	void cancelLike(int feedNo, String memberId);
 }
