21f7f551cb9cd72d1f12c2879614dd5930184137
jini4u <108965125+jini4u@users.noreply.github.com>
2022-11-10 16:58:15 +0900

    d

--- src/main/java/com/kosa/instagram/feed/service/IFeedService.java
+++ src/main/java/com/kosa/instagram/feed/service/IFeedService.java
@@ -21,7 +21,7 @@ public interface IFeedService {
 
 	void writeReply(int feedNo, String memberId, String replyContent);
 	List<MemberVo> searchListByKeyword(String keyword);
-	List<FeedVo> searchListByHashtag(String hashtag); 
+	List<String> searchListByHashtag(String hashtag); 
 	void deleteReply(int replyNo);
 	void increaseLike(int feedNo, String memberId, String logURI);
 	void decreaseLike(int feedNo, String memberId, String logURI);
